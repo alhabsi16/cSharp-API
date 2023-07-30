355015da5949c84e260c4d542a7bafa09ea0721d
alhabsi16 <mxmx8283@gmail.com>
2023-07-30 10:00:32 +0400

    country

---
+++ CountryInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_HttpRequests
+{
+    internal class CountryInfo
+    {
+        string officialName;
+        string capital;
+        Double area;
+
+        public CountryInfo(string officialName, string capital, Double area)
+        {
+            this.officialName = officialName;
+            this.capital = capital;
+            this.area = area;
+        }
+    }
+}
